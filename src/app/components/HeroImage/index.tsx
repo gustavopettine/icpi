b6c17ed04947a050e3875e63bc04a277ade954d6
gustavopettine <gupettine@gmail.com>
2025-06-22 21:29:41 -0300

    chore: remove comments

--- src/app/components/HeroImage/index.tsx
+++ src/app/components/HeroImage/index.tsx
@@ -4,17 +4,12 @@ import Image from 'next/image'
 export default function HeroImage() {
   return (
     <div className="max-w-4xl mx-auto">
-      {/* <div className="relative w-full h-48 sm:h-64 md:h-72 lg:h-80 xl:h-96 max-h-96"> */}
       <Image
         src={heroImage}
         alt="Educação inclusiva e inovadora - Instituto Conhecer Para Incluir"
-        //   fill
         className="object-cover max-h-96 rounded-xl shadow-md hover:shadow-lg transition-shadow duration-300"
         priority
-        // rounded-lg
-        // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 90vw, 80vw"
       />
-      {/* </div> */}
     </div>
   )
 }
