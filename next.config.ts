332ee9417e72637310970c06be22f7ca5b02f7de
gupettine <gupettine@gmail.com>
2025-06-28 15:47:43 -0300

    feat: add rewrite for projects route

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  async rewrites() {
+    return [
+      {
+        source: '/projetos',
+        destination: '/projects',
+      },
+    ]
+  },
+  // outros configs...
+}
 
-export default nextConfig;
+export default nextConfig
